bd93515c873efafdddf07774eb0298b07f04b823
Nouran Ali <nouran.cs.99@gmail.com>
2022-07-16 18:27:40 +0200

    Add all my files

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flipcard/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Matching Cards Game',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
